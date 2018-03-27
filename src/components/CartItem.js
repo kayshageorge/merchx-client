d9a1179a6ec45465bb1247f22520c4b3950175bd
Kaysha George <kayshageorge@icloud.com>
2018-03-27 16:56:36 -0700

    add state to cartitem component

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Row, Col } from 'antd';
 import { Product, Sku } from '../lib/requests';
 
 class CartItem extends React.Component {
-  async componentDidMount() {
-    let sku = {}
-    await Sku.one(this.props.item.sku_id).then(data => {
-      sku = data
+  constructor() {
+    super();
+    this.state = {
+      sku: {},
+      image: {}
+    };
+  }
+
+  componentDidMount() {
+    Sku.one(this.props.item.sku_id).then(data => {
+      this.setState({sku: data, image: data.product.images});
+      console.log('this sku', data);
       return data;
     })
-    console.log('this sku', sku);
   }
 
   render() {
-    const item = this.props.item
+    const sku = this.state.sku
+    console.log(this.state.image)
     return(
       <div>
-        <Card>
-          <h1>an item</h1>
+        <Card style={{height: '140px', padding: '0'}} bodyStyle={{padding: '0', alignSelf: 'center'}}>
+          <Row>
+              <Col span={6}>
+                <Card
+                   hoverable
+                   cover={<img alt="example" src={this.state.image} />}
+                   style={{ margin: "10px", padding: '0'}}
+                   bodyStyle={{padding: "0"}}
+                 >
+
+                </Card>
+              </Col>
+          </Row>
         </Card>
       </div>
     )
