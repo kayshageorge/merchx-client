ba423b13f04d56c0af50a97a06af2a223c4aef9f
Kaysha George <kayshageorge@icloud.com>
2018-03-31 23:38:30 -0700

    tweak payment form

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,44 +1,13 @@
 import React from 'react';
-import {Row, Col} from 'antd';
-import {CardNumberElement, CardExpiryElement, CardCVCElement, PostalCodeElement } from 'react-stripe-elements';
+import {CardElement} from 'react-stripe-elements';
 
 class CardSection extends React.Component {
   render() {
     return (
-      <div>
-        <Row style={{display: 'flex', justifyContent: 'center'}}>
-          <Col span={12}>
-            <label>
-              Card Number
-              <CardNumberElement style={{base: {fontSize: '18px'}}} />
-            </label>
-          </Col>
-        </Row>
-        <Row style={{display: 'flex', justifyContent: 'center'}}>
-          <Col>
-            <label>
-              Expiry Date
-              <CardExpiryElement style={{base: {fontSize: '18px'}}} />
-            </label>
-          </Col>
-        </Row>
-        <Row style={{display: 'flex', justifyContent: 'center'}}>
-          <Col>
-            <label>
-              CVC
-              <CardCVCElement style={{base: {fontSize: '18px'}}} />
-            </label>
-          </Col>
-        </Row>
-        <Row style={{display: 'flex', justifyContent: 'center'}}>
-          <Col>
-            <label>
-              Postal Code
-              <PostalCodeElement style={{base: {fontSize: '18px'}}} />
-            </label>
-          </Col>
-        </Row>
-      </div>
+      <label>
+        Card details
+        <CardElement style={{base: {fontSize: '18px'}}} />
+      </label>
     );
   }
 };
