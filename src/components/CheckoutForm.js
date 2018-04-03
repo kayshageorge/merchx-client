43e213220488d3f9b8b613465fec89abb09b1ffb
Kaysha George <kayshageorge@icloud.com>
2018-04-02 19:10:43 -0700

    styling for checkout page

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'antd';
 import { injectStripe } from 'react-stripe-elements';
 import CardSection from './CardSection';
 import CartHeader from './CartHeader';
@@ -9,23 +10,25 @@ import uiActions from '../actions/uiActions';
 class CheckoutForm extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
-    // debugger
     this.props.stripe.createToken().then(({token}) => {
       console.log('Received Stripe token:', token);
       Charge.create(token, this.props.total);
-
-
     });
 
   }
+  // Send all form inputs as params to charge request, then disect it into the pieces you need (ei: total, customer email and marketing prefs)
+  // after charge request, if complete, erase localstore and state store, and show pickup page w QR code and further instructions. If errors present, show errors on page
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <CartHeader />
-        <CardSection />
-        <button>Confirm order</button>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <CartHeader />
+          <CardSection />
+          <Button type="primary" style={{margin: 'auto'}}>Complete order</Button>
+        </form>
+        <img src='https://www.aida.rentals/credit_cards.png' style={{height: '40px', marginTop: '15px'}}/>
+      </div>
     );
   }
 }
